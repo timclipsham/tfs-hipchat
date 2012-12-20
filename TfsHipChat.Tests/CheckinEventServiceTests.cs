0963546690f77ca640d88d8b7d4492cbe21a8e3e
Tim Clipsham <tim.clipsham@gmail.com>
2012-12-20 17:38:00 +1100

    Add test for invalid xml for checkineventservice; add nsubstitute to project

---
+++ TfsHipChat.Tests/CheckinEventServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace TfsHipChat.Tests
+{
+    public class CheckinEventServiceTests
+    {
+        [Fact]
+        public void Notify_ShouldThrowException_WhenInvalidXmlData()
+        {
+            var notifier = Substitute.For<INotifier>();
+            var checkinEventService = new CheckinEventService(notifier);
+            const string eventXml = "invalid_xml";
+            const string tfsIdentityXml = "invalid_xml";
+
+            Assert.Throws<InvalidOperationException>(() => {
+                checkinEventService.Notify(eventXml, tfsIdentityXml);
+            });
+        }
+    }
+}
