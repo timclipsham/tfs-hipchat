efecc6d0a4577382684542d1e493320b0d4c6d70
Tim Clipsham <tim.clipsham@gmail.com>
2012-12-20 19:52:54 +1100

    Add hipchat notifier

---
+++ TfsHipChat/HipChatNotifier.cs
@@ -0,0 +1,24 @@
+﻿using HipChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfsHipChat
+{
+    public class HipChatNotifier : INotifier
+    {
+        private HipChatClient _hipChatClient;
+
+        public HipChatNotifier(HipChatClient hipChatClient)
+        {
+            _hipChatClient = hipChatClient;
+        }
+
+        public void SendMessage(string message)
+        {
+            _hipChatClient.SendMessage(message);
+        }
+    }
+}
