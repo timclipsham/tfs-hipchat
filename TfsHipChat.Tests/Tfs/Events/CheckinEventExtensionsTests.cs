1d1147b5a803a5ecdef4107e0fb2bdee09f0f395
Tim Clipsham <tim.clipsham@gmail.com>
2013-01-21 23:36:17 +1100

    Add extension method to get all versioned items of a checkin

--- TfsHipChat.Tests/Tfs/Events/CheckinEventExtensionsTests.cs
+++ TfsHipChat.Tests/Tfs/Events/CheckinEventExtensionsTests.cs
@@ -28,5 +28,56 @@ namespace TfsHipChat.Tests.Tfs.Events
 
             Assert.Equal(null, url);
         }
+
+        [Fact]
+        public void GetVersionedItems_ShouldReturnVersionedItems_WhenTheyExist()
+        {
+            var versionedItem = CreateVersionedItem();
+            var checkinEvent = new CheckinEvent { Artifacts = new ArrayList() };
+            checkinEvent.Artifacts.Add(versionedItem);
+
+            var versionedItems = checkinEvent.GetVersionedItems();
+
+            Assert.Contains(versionedItem, versionedItems);
+        }
+
+        [Fact]
+        public void GetVersionedItems_ShouldReturnCorrectNumberOfItems_WhenArtifactsOfDifferentTypesExist()
+        {
+            var versionedItem = CreateVersionedItem();
+            var otherItem = new ClientArtifact("http://some-tfs-server.com/url_to_other_item", "Changeset");
+            var checkinEvent = new CheckinEvent { Artifacts = new ArrayList() };
+            checkinEvent.Artifacts.Add(versionedItem);
+            checkinEvent.Artifacts.Add(otherItem);
+
+            var versionedItems = checkinEvent.GetVersionedItems();
+
+            Assert.Equal(1, versionedItems.Count);
+        }
+
+        [Fact]
+        public void GetVersionedItems_ShouldReturnAnEmptyCollection_WhenNoVersionedItemsExist()
+        {
+            var checkinEvent = new CheckinEvent { Artifacts = new ArrayList() };
+
+            var versionedItems = checkinEvent.GetVersionedItems();
+
+            Assert.Empty(versionedItems);
+        }
+
+        private static ClientArtifact CreateVersionedItem()
+        {
+            return new ClientArtifact
+                       {
+                           Url = "http://some-tfs-server.com/url_to_versioned_item",
+                           Type = "VersionedItem",
+                           Item = "MyFile.cs",
+                           Folder = "$/SomeTeamProject/TfsPathTo",
+                           TeamProject = "SomeTeamProject",
+                           ItemVersion = "10001",
+                           ChangeType = "edit",
+                           ServerItem = "$/SomeTeamProject/TfsPathTo/MyFile.cs"
+                       };
+        }
     }
 }
--- TfsHipChat/Tfs/Events/CheckinEventExtensions.cs
+++ TfsHipChat/Tfs/Events/CheckinEventExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.TeamFoundation.VersionControl.Common;
+﻿using System.Collections.Generic;
+using Microsoft.TeamFoundation.VersionControl.Common;
 using System.Linq;
 
 namespace TfsHipChat.Tfs.Events
@@ -10,6 +11,13 @@ namespace TfsHipChat.Tfs.Events
             return (from ClientArtifact ca in checkinEvent.Artifacts
                     where ca.Type == "Changeset"
                     select ca.Url).FirstOrDefault();
+        }
+
+        public static ICollection<ClientArtifact> GetVersionedItems(this CheckinEvent checkinEvent)
+        {
+            return (from ClientArtifact ca in checkinEvent.Artifacts
+                    where ca.Type == "VersionedItem"
+                    select ca).ToList();
         }
     }
 }
