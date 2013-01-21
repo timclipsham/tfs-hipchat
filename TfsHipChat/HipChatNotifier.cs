9ce4a34c78bb811061e47d767c207ca24989e080
Tim Clipsham <tim.clipsham@gmail.com>
2013-01-21 22:56:14 +1100

    Replaces the generated checkin url with the one generated by the server

--- TfsHipChat/HipChatNotifier.cs
+++ TfsHipChat/HipChatNotifier.cs
@@ -24,9 +24,8 @@ namespace TfsHipChat
 
         public void SendCheckinNotification(CheckinEvent checkinEvent)
         {
-            var changesetUrl = BuildChangesetUrl(checkinEvent.Number);
             var message = string.Format("{0} checked in changeset <a href=\"{1}\">{2}</a><br>{3}",
-                checkinEvent.CommitterDisplay, changesetUrl, checkinEvent.Number, checkinEvent.Comment);
+                checkinEvent.CommitterDisplay, checkinEvent.GetChangesetUrl(), checkinEvent.Number, checkinEvent.Comment);
             _hipChatClient.SendMessage(message, HipChatClient.BackgroundColor.yellow);
         }
 
@@ -34,11 +33,6 @@ namespace TfsHipChat
         {
             var message = string.Format("{0} (requested by {1})", buildCompletionEvent.Title, buildCompletionEvent.RequestedBy);
             _hipChatClient.SendMessage(message, HipChatClient.BackgroundColor.red);
-        }
-
-        private string BuildChangesetUrl(int changesetNumber)
-        {
-            return _tfsServerUrl + "_versionControl/changeset?id=" + changesetNumber;
         }
     }
 }
