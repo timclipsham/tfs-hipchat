d7b0d9913cfc854c75468629f48abe2d98ded102
Tim Clipsham <tim.clipsham@gmail.com>
2013-02-25 07:53:11 +1000

    Extract standard domain user name formatting

---
+++ TfsHipChat.Tests/Tfs/StandardDomainUserNameFormatterTests.cs
@@ -0,0 +1,52 @@
+﻿using TfsHipChat.Tfs;
+using Xunit;
+
+namespace TfsHipChat.Tests.Tfs
+{
+    public class StandardDomainUserNameFormatterTests
+    {
+        [Fact]
+        public void Valid_ShouldReturnTrue_WhenStandardDomainUserName()
+        {
+            const string userName = @"DOMAIN\user.name";
+            var formatter = new StandardDomainUserNameFormatter();
+
+            var valid = formatter.Valid(userName);
+
+            Assert.True(valid);
+        }
+
+        [Fact]
+        public void Valid_ShouldReturnFalse_WhenNonStandardDomainUserName()
+        {
+            const string userName = @"DOMAIN\uname1";
+            var formatter = new StandardDomainUserNameFormatter();
+
+            var valid = formatter.Valid(userName);
+
+            Assert.False(valid);
+        }
+
+        [Fact]
+        public void ToDisplayName_ShouldReturnFormattedDisplayName_WhenStandardDomainUserName()
+        {
+            const string userName = @"DOMAIN\user.name";
+            var formatter = new StandardDomainUserNameFormatter();
+
+            var displayName = formatter.ToDisplayName(userName);
+
+            Assert.Equal("User Name", displayName);
+        }
+
+        [Fact]
+        public void ToDisplayName_ShouldReturnGivenUserName_WhenNonStandardDomainUserName()
+        {
+            const string userName = @"DOMAIN\uname1";
+            var formatter = new StandardDomainUserNameFormatter();
+
+            var displayName = formatter.ToDisplayName(userName);
+
+            Assert.Equal(@"DOMAIN\uname1", displayName);
+        }
+    }
+}
---
+++ TfsHipChat/Tfs/StandardDomainUserNameFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace TfsHipChat.Tfs
+{
+    public class StandardDomainUserNameFormatter : IUserNameFormatter
+    {
+        // regex to match "DOMAIN\user.name"
+        private static readonly Regex StandardDomainUserName = new Regex(
+            @"^[^\\]+\\([^\.]+)\.([^\.]+)", RegexOptions.Compiled);
+
+        public bool Valid(string userName)
+        {
+            return StandardDomainUserName.Match(userName).Success;
+        }
+
+        public string ToDisplayName(string userName)
+        {
+            if (!Valid(userName))
+            {
+                return userName;
+            }
+
+            var match = StandardDomainUserName.Match(userName);
+            var displayName = match.Groups[1].Value + " " + match.Groups[2].Value;
+            return CultureInfo.GetCultureInfo("en-US").TextInfo.ToTitleCase(displayName);
+        }
+    }
+}
