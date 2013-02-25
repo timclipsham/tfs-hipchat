1ee3dea5e8200e2ffd39e62e3179233e40cef38e
Tim Clipsham <tim.clipsham@gmail.com>
2013-02-26 08:45:14 +1100

    Add tests for json config to team project mapping

---
+++ TfsHipChat.Tests/Configuration/TeamProjectMappingTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using TfsHipChat.Configuration;
+using Xunit;
+
+namespace TfsHipChat.Tests.Configuration
+{
+    public class TeamProjectMappingTests
+    {
+        [Fact]
+        public void TeamProjectName_ShouldReturnTeamProjectName_WhenValidJson()
+        {
+            var mapping = GetTeamProjectMapping();
+            Assert.Equal("TestProject", mapping.TeamProjectName);
+        }
+
+        [Fact]
+        public void HipChatRoomId_ShouldReturnHipChatRoomId_WhenValidJson()
+        {
+            var mapping = GetTeamProjectMapping();
+            Assert.Equal(1234, mapping.HipChatRoomId);
+        }
+
+        private static TeamProjectMapping GetTeamProjectMapping()
+        {
+            using (var reader = new JsonTextReader(new StreamReader(@"Configuration\TestConfig.json")))
+            {
+                return (new JsonSerializer()).Deserialize<TeamProjectMapping>(reader);
+            }
+        }
+    }
+}
