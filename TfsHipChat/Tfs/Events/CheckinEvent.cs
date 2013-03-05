74faf8538d75024faf2e0cfd98be46e9b62506d3
Tim Clipsham <tim.clipsham@gmail.com>
2013-03-05 22:42:33 +1100

    Remove unused using statements

--- TfsHipChat/Tfs/Events/CheckinEvent.cs
+++ TfsHipChat/Tfs/Events/CheckinEvent.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace TfsHipChat.Tfs.Events
